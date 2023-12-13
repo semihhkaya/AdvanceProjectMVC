fe6f6e8422861bb694e7c1548e6b380debb05d1b
DELL <DELL@SEMIH>
2023-12-13 04:31:52 +0300

    Login and Register Form UI

---
+++ AdvanceProjectMVC.UI/Controllers/AuthController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvanceProjectMVC.UI.Controllers
+{
+	public class AuthController : Controller
+	{
+		[HttpGet]
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult Register()
+		{
+			return View();
+		}
+	}
+}
